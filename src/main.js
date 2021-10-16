8c3c9dee979c8bf3edc640a330577620a678e0fa
anilcrk <you@example.com>
2021-10-16 05:42:02 +0300

    initial 1

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App.vue' // render olacak dosya
 
 new Vue({
   el: '#app',
+  // es6 function --> App render edilir.
   render: h => h(App)
 })
